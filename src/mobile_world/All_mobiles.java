6df371580133e6fb46c6eabc7b01d40c9f2d95e8
sanandh0111 <109470093+sanandh0111@users.noreply.github.com>
2022-09-22 10:02:18 +0530

    Update All_mobiles.java

--- src/mobile_world/All_mobiles.java
+++ src/mobile_world/All_mobiles.java
@@ -62,6 +62,7 @@ public class All_mobiles
 @Test(priority = 6)
           public void wrongSamsung() throws InterruptedException{
                 driver.findElement(By.id("myInput")).sendKeys("S@msung");
+        
                 Thread.sleep(1000);
                 // We cannot assert statement here because no error message is displayed
                 driver.findElement(By.id("myInput")).clear();
@@ -75,20 +76,23 @@ public class All_mobiles
              driver.findElement(By.id("myInput")).clear();
                 }
 @Test(priority = 8)
-          public void InvalidMob() throws InterruptedException {
-              driver.findElement(By.id("myInput")).sendKeys("Man");
-              Thread.sleep(1000);
-             //  We cannot assert statement here because no error message is displayed
-              driver.findElement(By.id("myInput")).clear();
-                }
-@Test(priority = 9)
          public void order() throws InterruptedException {
              driver.findElement(By.id("myInput")).sendKeys("Samsung Galaxy S21");
              Thread.sleep(1000);
              driver.findElement(By.xpath("//tbody/tr[1]/td[5]/a[1]")).click();
              Thread.sleep(1000);
-             // We cannot assert statement here because same page is opening again
-             driver.quit();
+             driver.findElement(By.id("myInput")).clear();
+             
             }
+@Test(priority = 9)
+public void InvalidMob() throws InterruptedException {
+    driver.findElement(By.id("myInput")).sendKeys("  Samsung");
+    String mobName = driver.findElement(By.xpath("//th[text()='Mobile Name']/../../../tbody/tr/td")).getText();
+    Thread.sleep(1000);
+   //  We cannot assert statement here because no error message is displayed
+    Assert.assertTrue(mobName.contains("Samsung"));
+    driver.findElement(By.id("myInput")).clear();
+    driver.quit();
             
         }
+}
